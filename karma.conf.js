97955df057c5a829b978dbdff9529090efc4975a
Justin Doherty <j.dohert@gmail.com>
2017-01-11 20:26:31 -0500

    Change lcov output dir

--- karma.conf.js
+++ karma.conf.js
@@ -72,7 +72,7 @@ module.exports = function (config) {
       },
       reports: {
         'text-summary': '',
-        'lcovonly': './coverage'
+        'lcovonly': './coverage/lcov'
       }
     }
   })
